6887027598a156084d01788d714e69f4bb829578
GerazimCode <gerazimcode@gmail.com>
2021-09-26 21:41:38 -0400

    add start and end index variable and iteration

--- js/script.js
+++ js/script.js
@@ -15,13 +15,26 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 function showPage(list, page){
-   let startIndex;
-   let endIndex;
+
+   // following variables represent the first and last student of the list.
+   let itemsPerPage = list.length;
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+
+   let studentList = document.querySelector(".student-list");
+   studentList.innerHTML = "";
+
+   for(let i=0; i<list.length; i++){
+      if(i >= startIndex && i < endIndex){
+
+      }
+   }
 
    
 }
+
+showPage(data, 1);
 
 
 
